5be3a719bc821cf11c9bca92888f1a1fcb7ed639
Felipe <felipeslopes2010@hotmail.com>
2021-10-15 23:56:49 -0300

    Adicionando Mockito na classe TransacaoService

--- src/main/java/br/com/alura/carteira/service/TransacaoService.java
+++ src/main/java/br/com/alura/carteira/service/TransacaoService.java
@@ -1,5 +1,7 @@
 package br.com.alura.carteira.service;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,31 +12,47 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.alura.carteira.dto.TransacaoDto;
 import br.com.alura.carteira.dto.TransacaoFormDto;
 import br.com.alura.carteira.modelo.Transacao;
+import br.com.alura.carteira.modelo.Usuario;
 import br.com.alura.carteira.repository.TransacaoRepository;
+import br.com.alura.carteira.repository.UsuarioRepository;
 
 @Service
 public class TransacaoService {
 
 	@Autowired
-	TransacaoRepository transacaoRepository;
+	TransacaoRepository repository;
+	
+	@Autowired
+	UsuarioRepository usuarioRepository;
 
 	private ModelMapper modelMapper = new ModelMapper();
 
 	public Page<TransacaoDto> listar(Pageable paginacao) {
 
-		Page<Transacao> transacoes = transacaoRepository.findAll(paginacao);
-		return transacoes.map(t -> modelMapper.map(t, TransacaoDto.class));
+		return repository
+				.findAll(paginacao)
+				.map(t -> modelMapper.map(t, TransacaoDto.class));
 
 	}
 
 	@Transactional
 	public TransacaoDto cadastrar(TransacaoFormDto dto) {
-
-		Transacao transacao = modelMapper.map(dto, Transacao.class);
-		transacao.setId(null);
-
-		transacaoRepository.save(transacao);
-		return modelMapper.map(transacao, TransacaoDto.class);
-
+		Long idUsuario = dto.getUsuarioId();
+		
+		try {
+			Usuario usuario = usuarioRepository.getById(idUsuario);
+			Transacao transacao = modelMapper.map(dto, Transacao.class);
+			transacao.setId(null);
+			transacao.setUsuario(usuario);
+			
+			repository.save(transacao);
+			
+			return modelMapper.map(transacao, TransacaoDto.class);
+					
+		} catch (EntityNotFoundException e) {
+			
+			throw new IllegalArgumentException("usuario inexistente!");
+			
+		}
 	}
 }
